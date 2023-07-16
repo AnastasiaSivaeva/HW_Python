832fb78043350ffdac6905194daa8d247c72e585
Anastasia <anastasia.koles@gmail.com>
2023-07-16 17:52:58 +0300

    the task for HW 8 was completed

---
+++ HW_8/Task1.py
@@ -0,0 +1,11 @@
+#Напишите функцию, которая преобразует pickle файл хранящий список словарей в табличный csv файл. 
+#Для тестированию возьмите pickle версию файла из предыдущей задачи. Функция должна извлекать ключи словаря 
+#для заголовков столбца из переданного файла.
+
+import pickle
+import pandas
+
+with open("Task5.pickle", "rb") as f:
+    object1 = pickle.load(f)
+df = pandas.DataFrame(object1)
+df.to_csv(r'file.csv')
