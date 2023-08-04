5eeeda626a233f3ea1d3f4c166a311082d0b28f7
Anastasia <anastasia.koles@gmail.com>
2023-08-04 19:54:51 +0300

    the task for HW 14 was completed

--- HW_14/Exceptions.py
+++ HW_14/Exceptions.py
@@ -1,6 +1,8 @@
 class OwnException(Exception):
     pass
 
+
+
 class LevelError(OwnException):
     def __init__(self, user_level, admin_level):
         self.user_level = user_level
@@ -8,7 +10,7 @@ class LevelError(OwnException):
 
     def __str__(self):
         return f"""Отказано в доступе!
-Ваш уровень доступа({self.user_level}) должен быть ниже чем, уровень доступа администратора({self.admin_level})"""
+Ваш уровень доступа({self.user_level}) должен быть ниже уровня доступа администратора({self.admin_level})"""
     
 class NotAllowedError(OwnException):
     def __init__(self, name, id_):
